eebe85763590d005cd1af9d1c499bead88825bc7
Alfonso I. Raggio <projectja@gmail.com>
2021-04-03 10:08:47 +0200

    primer commit

---
+++ webapp/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def bienvenido(request):
+    return HttpResponse('Hola mundo desde Django')
+
+def despedirse(request):
+    return HttpResponse('Despedida de Django')
+
+
+def contacto(request):
+    return HttpResponse('contacto')
